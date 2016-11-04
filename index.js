162d160ef017d6ea2e16eb17da895071150faae5
Isuru <github email address>
2016-11-04 12:36:55 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+
+function nestedTarget(){
+  return document.querySelector('#nested .target');
+}
+
+function deepestChild(){
+  return document.querySelector('#grand-node div ');
+}
+
+function increaseRankBy(n){
+  const rankedList = document.querySelectorAll('.ranked-list');
+
+  for (let i = 0, l = rankedList.length; i < l; i++) {
+      let children = rankedList[i].children
+
+  for (let i = 0, l = children.length; i < l; i++) {
+  children[i].innerHTML = parseInt(children[i].innerHTML)+n
+  }
+  }
+}
